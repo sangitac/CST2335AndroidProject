d3060554d26d2cd3376bbb416767553d2471acb8
sangitac <sangitac@gmal.com>
2017-04-11 19:45:49 -0400

    added async task and time picker

--- app/src/main/java/com/example/sangita/androidproject/IndoorTempFragment.java
+++ app/src/main/java/com/example/sangita/androidproject/IndoorTempFragment.java
@@ -1,12 +1,34 @@
 package com.example.sangita.androidproject;
 
 
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TimePicker;
+import java.util.Calendar;
+import android.os.Bundle;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+
+import java.util.Calendar;
 
 
 /**
@@ -14,10 +36,13 @@ import android.view.ViewGroup;
  */
 public class IndoorTempFragment extends Fragment {
 
+    static EditText inputTime;
+
 
     public IndoorTempFragment() {
         // Required empty public constructor
     }
+
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
@@ -25,10 +50,85 @@ public class IndoorTempFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_indoor_temp, container, false);
+        //return inflater.inflate(R.layout.fragment_indoor_temp, container, false);
+        final View view = inflater.inflate(R.layout.fragment_indoor_temp, container, false);
+
+        inputTime = (EditText) view.findViewById(R.id.input_time);
+
+
+        inputTime.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog(v);
+                showDatePickerDialog(v);
+            }
+        });
+
+        return view;
     }
 
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            inputTime.setText(day + "/" + (month + 1) + "/" + year);
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "timePicker");
+    }
+
+
+    public static class TimePickerFragment extends DialogFragment implements
+            TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+
+            // Create a new instance of TimePickerDialog and return it
+          /*  TimePickerDialog mTimePicker =new TimePickerDialog(getActivity(), this, hour, minute,
+                   false);
+            return mTimePicker;*/
+
+           return new TimePickerDialog(getActivity(), this, hour, minute,
+              DateFormat.is24HourFormat(getActivity()));
+        }
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            inputTime.setText(inputTime.getText() + " - "+ hourOfDay + ":" + minute);
+        }
+    }
+
+
 }
+
